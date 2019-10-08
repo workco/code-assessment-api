b421d6806fbac6d4e0fa35984b92b6aa15294a2d
Nick Lee <nick.lee@work.co>
2019-10-08 16:54:11 -0400

    enabling GraphQL playground and introspection in production

--- graphql/index.js
+++ graphql/index.js
@@ -3,4 +3,9 @@ const { ApolloServer } = require("apollo-server-hapi");
 const typeDefs = require("./defs");
 const resolvers = require("./resolvers");
 
-module.exports = new ApolloServer({ typeDefs, resolvers });
+module.exports = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
